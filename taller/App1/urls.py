66e09700ee64f26f7b004a55de535f7aa83ebd4b
geromt <gerardomartinez205@gmail.com>
2025-03-25 21:43:16 -0600

    feat: Add view to retrieve all the autors and by id as json

--- taller/App1/urls.py
+++ taller/App1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from App1.views import IndexView, AutorView
+from App1.views import IndexView, AutorView, AutorAPIJson, AutorById
 
 urlpatterns = [
     path('', IndexView),
-    path('autor/<int:id>', AutorView)
+    path('autor/<int:id>', AutorView),
+    path('api/autores-json', AutorAPIJson, name='autores_api'),
+    path('api/autor-by-id/<int:id>', AutorById, name='autor_by_id')
 ]
--- taller/App1/views.py
+++ taller/App1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import AutorModel, FraseModel
+from django.core.serializers import serialize
 
 
 # Create your views here.
@@ -15,3 +16,17 @@ def IndexView(request):
 def AutorView(request, id):
     autor = get_object_or_404(AutorModel, id=id)
     return render(request, "autor.html", {"objeto": autor})
+
+# Vistas API
+
+
+def AutorAPIJson(request):
+    autores = AutorModel.objects.all().order_by("id")
+    autores_serialize = serialize('json', autores)
+    return HttpResponse(autores_serialize, content_type='application/json', status=200)
+
+def  AutorById(request, id):
+    autor = AutorModel.objects.filter(id=id)
+    print("Holi ", autor)
+    autor_serialize = serialize('json', autor)
+    return HttpResponse(autor_serialize, content_type='application/json', status=200)
